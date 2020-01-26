0ed5ed41534e8dae40b51ab819114539b82f4a56
Lucile Lévêque <lululeveq@hotmail.fr>
2020-01-26 23:09:00 +0100

    Squelette des classes de réactions

---
+++ reactions.py
@@ -0,0 +1,75 @@
+class KaamelottReactions():
+    def mecreant(self):
+        pass
+
+    def pasfaux(self):
+        pass
+
+
+class LivreDOrReactions():
+    def livredor(self):
+        pass
+
+    def livredorauteur(self):
+        pass
+
+    def livredorhelp(self):
+        pass
+
+
+class BasicReactions():
+    def ping(self):
+        pass
+
+    def jeu(self):
+        pass
+
+    def allemand(self):
+        pass
+
+    def degueu(self):
+        pass
+
+    def toimeme(self):
+        pass
+
+    def info(self):
+        pass
+
+    def help(self):
+        pass
+
+    def humain(self):
+        pass
+
+
+class PoliteReactions():
+    def merde(self):
+        pass
+
+    def con(self):
+        pass
+
+    def cthulhu(self):
+        pass 
+
+    def mauvaisCthulhu(self):
+        pass
+
+    def fucking(self):
+        pass
+
+    def tanche(self):
+        pass
+
+    def merci(self):
+        pass
+    
+    def silence(self):
+        pass
+    
+    def tagueule(self):
+        pass
+
+    def nuit(self):
+        pass
