b621e0dca67e619811910e935a64830cb070e0f8
Lucile Lévêque <lululeveq@hotmail.fr>
2020-01-27 00:19:51 +0100

    La class Reactions traite universellement les réactions + fichier contenant tous les mots-clefs de réaction

--- reactions.py
+++ reactions.py
@@ -1,75 +1,15 @@
-class KaamelottReactions():
-    def mecreant(self):
+import re
+
+class Reactions():
+    def __init__(self):
+        # Chargement des mots-clefs
         pass
 
-    def pasfaux(self):
+    def search_key_word(self):
         pass
 
-
-class LivreDOrReactions():
-    def livredor(self):
+    def send_response(self):
         pass
 
-    def livredorauteur(self):
-        pass
-
-    def livredorhelp(self):
-        pass
-
-
-class BasicReactions():
-    def ping(self):
-        pass
-
-    def jeu(self):
-        pass
-
-    def allemand(self):
-        pass
-
-    def degueu(self):
-        pass
-
-    def toimeme(self):
-        pass
-
-    def info(self):
-        pass
-
-    def help(self):
-        pass
-
-    def humain(self):
-        pass
-
-
-class PoliteReactions():
-    def merde(self):
-        pass
-
-    def con(self):
-        pass
-
-    def cthulhu(self):
-        pass 
-
-    def mauvaisCthulhu(self):
-        pass
-
-    def fucking(self):
-        pass
-
-    def tanche(self):
-        pass
-
-    def merci(self):
-        pass
-    
-    def silence(self):
-        pass
-    
-    def tagueule(self):
-        pass
-
-    def nuit(self):
-        pass
+    # Ici on définit les fonctions des réponses complexes:
+    # TODO
