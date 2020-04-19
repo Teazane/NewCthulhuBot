3b065fc1357ac4601727595bcd6ffeea881a2dad
Lucile Lévêque <lululeveq@hotmail.fr>
2020-04-19 21:07:18 +0200

    Traitement des réactions

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
-import discord
+import discord, random, time
+from reactions import Reactions
 
 class Bot(discord.Client):
+    # Explication du mécanisme asynchrone Python : https://stackabuse.com/python-async-await-tutorial/
+
+    # Initialisation du client avec création des réactions (chargement des listes, ...)
+    def __init__(self):
+        self.reactions = Reactions()
 
     async def on_ready(self):
-        print("Cthulhu bot is now logged as " + str(self.user))
+        print("Cthulhu bot is now ready as " + str(self.user))
+    
+    async def on_connect(self):
+        print("Cthulhu bot is now connected as " + str(self.user))
+        for chan in self.guilds[0].channels:
+            if chan.name == "general":
+                await chan.send("Salut les p'tits potes !")
 
     async def on_message(self, message):
         # Si l'auteur est un bot, on ne répond pas
         if message.author.bot:
             return
-        pass
+        else:
+            you_are_taunt = random.randint(0,50)
+            if you_are_taunt == 0:
+                await message.channel.send(self.reactions.toi_meme_repeat(message.content))
+            else:
+                await message.channel.send(self.reactions.search_key_word(message.content))
 
     async def on_disconnect(self):
         print("Cthulhu bot has been disconnected")
+
+    def muted_for_15_minutes(self):
+        time.sleep(900)
+        self.reactions.muted_state = False
--- reactions.py
+++ reactions.py
@@ -1,15 +1,55 @@
-import re
+import re, json, random
 
 class Reactions():
     def __init__(self):
         # Chargement des mots-clefs
-        pass
+        with open("match_pattern.json") as file: 
+            data = file.read()
+        self.data_json = json.loads(data)
+        # Liste de patterns (associés à leur entrée JSON)
+        self.pattern_list = {}
+        for entry in self.data_json:
+            self.pattern_list[entry["pattern"]] = entry
+        # Etat mute initialisé à "faux"
+        self.muted_state = False
 
-    def search_key_word(self):
-        pass
+    # Envoie "Toi-même 'spèce d[e']" + le dernier mot de la phrase
+    def toi_meme_repeat(self, received_msg):
+        repeat_world = re.search("^.* ([A-Za-zzéêèôîïëüö]{4,}).*$", received_msg).group(1)
+        answer_msg = "Toi-même, 'spèce d"
+        # Le mot commence-t-il par une voyelle ?
+        if re.search("^[éêèîêûïüëöaeiouyAEIOUY]", repeat_world) is not None:
+            return answer_msg + "'" + repeat_world + " !"
+        else:
+            return answer_msg + "e" + repeat_world + " !"
 
-    def send_response(self):
-        pass
+    # Recherche de mots-clefs et renvoi de la réponse correspondante
+    def search_key_word(self, received_msg):
+        for key_word in self.pattern_list.keys():
+            # Di un mot-clef est matché dans le message reçu
+            if re.search(key_word, received_msg) is not None:
+                response = self.data_json[self.pattern_list[key_word]]["response"]
+                if response["type"] == "simple":
+                    answer_range = len(response["message"])
+                    return response["message"][random.randint(0,answer_range)]
+                elif response["type"] == "complex":
+                    case = response["case"]
+                    # Ici on définit les fonctions des réponses complexes:
+                    if case == "silence" or case == "gueule":
+                        self.muted_state = True
+                        answer_range = len(response["message"])
+                        return response["message"][random.randint(0,answer_range)]
+                    elif case == "livredor":
+                        is_there_author = re.search("auteur=(.*)$", received_msg)
+                        quote_author = ""
+                        if is_there_author is not None:
+                            quote_author = is_there_author.group(1)
+                        return self._livre_dor(quote_author)
+                    else:
+                        raise Exception("The answer complex type case has not been recognized.")
+                else:
+                    raise Exception("The answer type has not been recognized.")
 
-    # Ici on définit les fonctions des réponses complexes:
-    # TODO
+    # Traitement des requêtes Livre d'Or
+    def _livre_dor(self, author):
+        return "A venir."
